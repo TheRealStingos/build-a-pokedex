8269c89eb64cc183eb8d01e6f93bd5ec8a29f3d6
TheRealStingos <justmo10@gmail.com>
2026-06-15 16:30:00 -0400

    start map function

--- src/command.ts
+++ src/command.ts
@@ -1,5 +1,6 @@
 import { commandExit } from "./command_exit.js";
 import { commandHelp } from "./command_help.js";
+import { commandMap } from "./command_map.js";
 import { CLICommand } from "./state.js";
 
 
@@ -14,6 +15,11 @@ export function getCommands(): Record<string, CLICommand> {
             name: "help",
             description: "Displays a help message",
             callback: commandHelp,
+        },
+        map: {
+            name: "map",
+            description: "Display a map",
+            callback: commandMap,
         }
     }
 }
