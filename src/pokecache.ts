b043c7eaaa705308246f6c17318f5e28a63b28cc
TheRealStingos <justmo10@gmail.com>
2026-06-17 16:47:31 -0400

    add pokecache.ts

---
+++ src/pokecache.ts
@@ -0,0 +1,44 @@
+export type CacheEntry<T> = {
+    createdAt: number
+    val: T
+}
+
+export class Cache {
+    #cache = new Map<string, CacheEntry<any>>();
+    #reapIntervalId: NodeJS.Timeout | undefined;
+    #interval: number;
+
+    constructor(interval: number) {
+        this.#interval = interval;
+    }
+
+    add<T>(key: string, val: T) {
+        this.#cache.set(key, { createdAt: Date.now(), val });
+    }
+
+    get<T>(key: string) {
+        if (!this.#cache.get(key)) {
+            return undefined
+        }
+        return this.#cache.get(key)
+    }
+
+    #reap() {
+        for (const [key, entry] of this.#cache) {
+            if (entry.createdAt < (Date.now() - this.#interval)) {
+                this.#cache.delete(key)
+            }
+        }
+    }
+
+    #startReapLoop() {
+        this.#reapIntervalId = setInterval(() => {
+            this.#reap()
+        }, this.#interval)
+    }
+
+    stopReapLoop() {
+        clearInterval(this.#reapIntervalId)
+        this.#reapIntervalId = undefined
+    }
+}
